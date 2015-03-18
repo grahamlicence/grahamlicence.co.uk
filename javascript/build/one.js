926bdb614fc8f1dc836efe5f6c7301c91372368b
Graham Licence <graham.licence@akqa.com>
2015-03-18 11:35:20 +0000

    Layout update

--- javascript/build/one.js
+++ javascript/build/one.js
@@ -1,1 +1,25 @@
-console.log('test1')
+// temp test
+(function () {
+	var $links = $('.main-nav a'),
+		page;
+
+	function loadPage (e) {
+		e.preventDefault();
+		var link = e.target.href,
+			loc = document.location.href;
+
+		if (link === loc) {
+			return;
+		}
+
+		$.ajax(
+			{url: link}
+		).done(function (resp) {
+			console.log(resp)
+		})
+
+	}
+
+	$links.on('click', loadPage);
+
+})();
