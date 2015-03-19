bbed4e1d10b3ecae2dda427c4c4895a747b689e8
Graham Licence <git@grahamlicence.co.uk>
2015-03-19 12:15:25 +0000

    Ajax testing

--- dist/javascript/build/one.js
+++ dist/javascript/build/one.js
@@ -1,11 +1,13 @@
 // temp test
 (function () {
 	var $links = $('.main-nav a'),
+		hasHistory = ("pushState" in history),
 		page;
 
 	function loadPage (e) {
 		e.preventDefault();
-		var link = e.target.href,
+		var $main = $('.main-content'),
+			link = e.target.href,
 			loc = document.location.href;
 
 		if (link === loc) {
@@ -15,7 +17,19 @@
 		$.ajax(
 			{url: link}
 		).done(function (resp) {
-			console.log(resp)
+			// console.log(resp)
+			var $resp = $(resp),
+				$content = $resp.find('.main-content'),
+				title = $resp.find('title').text;
+
+			console.log($content)
+			$main.replaceWith($content);
+
+			// update page and history
+			document.title = title;
+			if (hasHistory) {
+				history.pushState(null, title, link);
+			}
 		})
 
 	}
