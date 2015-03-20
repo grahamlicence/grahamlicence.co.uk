498def60356914b7cc31f22f9a88e73baa9ccd14
Graham Licence <git@grahamlicence.co.uk>
2015-03-20 14:37:55 +0000

    Navigation template work

--- src/helpers/helpers.js
+++ src/helpers/helpers.js
@@ -9,4 +9,11 @@ module.exports.register = function (Handlebars, options) {
     }
   });
 
+  Handlebars.registerHelper('if_eq', function(a, b, opts) {
+    if(a == b)
+        return opts.fn(this);
+    else
+        return opts.inverse(this);
+});
+
 };
