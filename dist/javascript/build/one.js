04a62fb552769614cb0757cd3acb6211924e5e21
Graham Licence <git@grahamlicence.co.uk>
2015-03-24 22:44:20 +0000

    navigation updates

--- dist/javascript/build/one.js
+++ dist/javascript/build/one.js
@@ -1,17 +1,22 @@
 // temp test
 (function () {
-	var $links = $('.main-nav a'),
+	var $navLinks = $('.main-nav a'),
 		hasHistory = ("pushState" in history),
 		page;
+
 
 	function loadPage (e) {
 		e.preventDefault();
 		var $main = $('.main-content'),
+			$nav = $('.main-nav'),
 			link = e.target.href,
 			loc = document.location.href;
 
 		if (link === loc) {
 			return;
+		}
+
+		function setSubNav () {
 		}
 
 		$.ajax(
@@ -24,7 +29,7 @@
 
 			// console.log(resp)
 			$main.replaceWith($content);
-
+			
 			// update page and history
 			document.title = title;
 			if (hasHistory) {
@@ -34,7 +39,9 @@
 
 	}
 
-	$links.on('click', loadPage);
+	// add event handlers
+	$navLinks.on('click', loadPage);
+	$(document).on('click', '.sub-nav a', loadPage);
 
 
 })();
