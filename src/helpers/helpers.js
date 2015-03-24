56e931490fa490a8dede43dea410ec1667e67454
Graham Licence <git@grahamlicence.co.uk>
2015-03-24 10:12:46 +0000

    Grouping test

--- src/helpers/helpers.js
+++ src/helpers/helpers.js
@@ -1,19 +1,20 @@
 module.exports.register = function (Handlebars, options) {
-  'use strict';
+    'use strict';
 
-  Handlebars.registerHelper('replaceStr', function (haystack, needle, replacement) {
-    if (haystack && needle) {
-      return haystack.replace(needle, replacement);
-    } else {
-      return '';
-    }
-  });
+    Handlebars.registerHelper('replaceStr', function (haystack, needle, replacement) {
+        if (haystack && needle) {
+            return haystack.replace(needle, replacement);
+        } else {
+            return '';
+        }
+    });
 
-  Handlebars.registerHelper('if_eq', function(a, b, opts) {
-    if(a == b)
-        return opts.fn(this);
-    else
-        return opts.inverse(this);
-});
+    Handlebars.registerHelper('if_eq', function(a, b, opts) {
+        if (a == b) {
+            return opts.fn(this);
+        } else {
+            return opts.inverse(this);
+        }
+    });
 
 };
